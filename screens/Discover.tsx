36138c9f8616bf6971b8513892d6819e6298b2fd
Michał Bryk <galeriakoloru90210@gmail.com>
2023-02-26 18:17:47 +0100

    configure .env file

--- screens/Discover.tsx
+++ screens/Discover.tsx
@@ -1,18 +1,43 @@
-import { View, Text } from "react-native";
+import { View, Text, SafeAreaView, Image } from "react-native";
 import React from "react";
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
 import { useNavigation } from "@react-navigation/native";
 
 //external imports
 import { StackParamList } from "../App";
+import { Avatar } from "../assets";
+import { GOOGLE_PLACES_API_KEY } from "@env";
 
 export type NavigationProp = NativeStackNavigationProp<StackParamList, "Discover">;
 
 const Discover = () => {
+  const navigation = useNavigation<NavigationProp>();
   return (
-    <View>
-      <Text>Discover</Text>
-    </View>
+    <SafeAreaView className="flex-1 bg-white relative">
+      <View className="flex-row items-center justify-between px-8">
+        <View>
+          <Text className="text-[38px] text-[#0B646B] font-bold">Discover</Text>
+          <Text className="text-[#527283] text-[30px]">the beauty today</Text>
+        </View>
+        <View className="w-12 h-12 bg-gray-400 rounded-md items-center justify-center shadow-lg">
+          <Image source={Avatar} className="w-full h-full rounded-md object-cover" />
+        </View>
+      </View>
+      <View className="flex-row items-center bg-white mx-4 rounded-xl py-1 px-4 shadow-lg mt-4">
+        <GooglePlacesAutocomplete
+          placeholder="Search"
+          onPress={(data, details = null) => {
+            // 'details' is provided when fetchDetails = true
+            console.log(data, details);
+          }}
+          query={{
+            key: GOOGLE_PLACES_API_KEY!,
+            language: "pl",
+          }}
+        />
+      </View>
+    </SafeAreaView>
   );
 };
 
