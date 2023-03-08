945aacb580bb98d64cd5f36802507aecfd7b04f5
Michał Bryk <galeriakoloru90210@gmail.com>
2023-03-08 22:11:16 +0100

    add photo ui to item screen

--- screens/ItemScreen.tsx
+++ screens/ItemScreen.tsx
@@ -1,4 +1,4 @@
-import { View, Text, SafeAreaView } from "react-native";
+import { View, Text, SafeAreaView, ScrollView, Image, TouchableOpacity } from "react-native";
 import React from "react";
 import { StackParamList } from "../App";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
@@ -11,10 +11,24 @@ const ItemScreen = ({ route }: Props) => {
 
   const data = route?.params?.param;
 
-  console.log(data);
   return (
-    <SafeAreaView>
-      <Text>ItemScreen</Text>
+    <SafeAreaView className="flex-1 bg-white relative">
+      <ScrollView className="flex-1 px-4 py-6">
+        <View className="relative bg-white shadow-lg">
+          <Image
+            source={{
+              uri: data?.photo?.images?.large?.url
+                ? data?.photo?.images?.large?.url
+                : "https://cdn.pixabay.com/photo/2015/10/30/12/22/eat-1014025_1280.jpg",
+            }}
+            className="w-full h-72 object-cover rounded-2xl"
+          />
+          <View className="absolute flex-row inset-x-0 top-5 justify-between px-6">
+            <TouchableOpacity className="w-10 h-10 rounded-md items-center justify-center bg-white"></TouchableOpacity>
+            <TouchableOpacity className="w-10 h-10 rounded-md items-center justify-center bg-[#06B2BE]"></TouchableOpacity>
+          </View>
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 };
