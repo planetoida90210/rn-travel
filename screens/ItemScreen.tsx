39e27e6a03e01a796eae0b71a9a24a260db43a60
Michał Bryk <galeriakoloru90210@gmail.com>
2023-03-09 22:16:51 +0100

    build ui for itemscreen

--- screens/ItemScreen.tsx
+++ screens/ItemScreen.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import { StackParamList } from "../App";
 import { NativeStackNavigationProp, NativeStackScreenProps } from "@react-navigation/native-stack";
 import { useNavigation } from "@react-navigation/native";
-import { FontAwesome5 } from "@expo/vector-icons";
+import { FontAwesome, FontAwesome5, MaterialIcons } from "@expo/vector-icons";
 
 type Props = NativeStackScreenProps<StackParamList, "ItemScreen">;
 
@@ -42,6 +42,58 @@ const ItemScreen = ({ route }: Props) => {
               />
             </TouchableOpacity>
           </View>
+          <View className="absolute flex-row inset-x-0 bottom-5 justify-between px-4">
+            <View className="flex-row space-x-2 items-center">
+              <Text className="text-[10px] font-bold text-gray-100">{data?.price_level}</Text>
+              <Text className="text-[16px] font-bold text-gray-100">{data?.price}</Text>
+            </View>
+            <View className="px-2 py-1 rounded-md bg-teal-100">
+              <Text>{data?.open_now_text}</Text>
+            </View>
+          </View>
+        </View>
+        <View className="mt-6">
+          <Text className="text-[#428288] text-[22px] font-bold">{data?.name}</Text>
+          <View className="flex-row items-center space-x-2 mt-2">
+            <FontAwesome name="map-marker" size={25} color="#8C9EA6" />
+            <Text className="text-[#8C9EA6] text-[16px] font-bold">{data?.location_string}</Text>
+          </View>
+        </View>
+        <View className="mt-4 flex-row items-center justify-between">
+          {data?.rating && (
+            <View className="flex-row items-center space-x-2">
+              <View className="w-12 h-12 rounded-2xl bg-red-100 items-center justify-center shadow-md">
+                <FontAwesome name="star" size={24} color="#D58574" />
+              </View>
+              <View>
+                <Text className="text-[#515151]">{data?.rating}</Text>
+                <Text className="text-[#515151]">Ratings</Text>
+              </View>
+            </View>
+          )}
+          {data?.price_level && (
+            <View className="flex-row items-center space-x-2">
+              <View className="w-12 h-12 rounded-2xl bg-red-100 items-center justify-center shadow-md">
+                <MaterialIcons name="attach-money" size={24} color="black" />
+              </View>
+              <View>
+                <Text className="text-[#515151]">{data?.price_level}</Text>
+                <Text className="text-[#515151]">Price Level</Text>
+              </View>
+            </View>
+          )}
+
+          {data?.bearing && (
+            <View className="flex-row items-center space-x-2">
+              <View className="w-12 h-12 rounded-2xl bg-red-100 items-center justify-center shadow-md">
+                <FontAwesome5 name="map-signs" size={24} color="black" />
+              </View>
+              <View>
+                <Text className="text-[#515151] capitalize">{data?.bearing}</Text>
+                <Text className="text-[#515151]">Bearing</Text>
+              </View>
+            </View>
+          )}
         </View>
       </ScrollView>
     </SafeAreaView>
